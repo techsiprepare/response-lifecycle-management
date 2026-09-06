7dd6151d43dc7b30c26916f0dd126ca2d3c08879
Moisés Emanuel <moisesemanuelreisdacruz@hotmail.com>
2026-09-06 15:42:20 -0300

    fix: corrigindo formato de data para o reenvio também

--- BE-Model-Reenvio.js
+++ BE-Model-Reenvio.js
@@ -1,11 +1,18 @@
 class Reenvio {
   constructor(dados = {}) {
-    this.carimboDataHora = dados.carimboDataHora;
+    this.carimboDataHora = this._formatarDataHora(dados.carimboDataHora);
     this.emailPessoal = dados.emailPessoal;
     this.emailInstitucional = dados.emailInstitucional;
     this.ticket = dados.ticket;
     this.urlAtualizada = dados.urlAtualizada;
     this.autorizacaoAtualizada = dados.autorizacaoAtualizada;
     this.descricao = dados.descricao;
+  }
+
+  _formatarDataHora(valor) {
+    if (!valor) return '';
+    return valor instanceof Date
+      ? new Intl.DateTimeFormat('pt-BR', { dateStyle: 'short', timeStyle: 'medium' }).format(valor)
+      : String(valor);
   }
 }
